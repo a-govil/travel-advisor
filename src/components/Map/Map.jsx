a26801100587d8b353874c301ec98711c15751f4
Anant Govil <44673944+a-govil@users.noreply.github.com>
2023-01-02 19:35:24 +0530

    Header

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import googleMapReact from 'google-map-react';
+import {Paper,Typography, useMediaQuery} from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/lab';
+import Rating from '@material-ui/lab';
+import useStyles from'./styles';
+
+const Map = () => {
+    const classes = useStyles();
+    const isMobile=useMediaQuery('(min-width:600px)');
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+            bootstrapURLKeys={{key:''}}
+            defaultCenter={}/>
+            </GoogleMapReact>
+        </div>
+    );
+}
+
+export default Map;
